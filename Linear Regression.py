9d843d1c0342e032b5dd486c32ca5ea5c473deaf
Abhivyakt Bhati <abhivyaktbhati1102@gmail.com>
2024-03-19 14:34:58 +0530

    improved notation

--- Linear Regression.py
+++ Linear Regression.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression 
 
 
 
@@ -11,17 +12,17 @@ data2 = pd.read_csv("data2.csv")
 # plt.scatter(data2.X, data2.Y)
 # plt.show()
 
-def loss_function(m, b, points):
+def loss_function(m, c, points):
     total_error = 0
     for i in  range(len(points)):
         x = points.iloc[i].X
         y = points.iloc[i].Y        
-        total_error += (y - ( m * x * b)) ** 2
+        total_error += (y - ( m * x * c)) ** 2
     total_error / float(len(points))
 
-def gradient_descent(m_now, b_now, points, L):
-    m_gradient = 0
-    b_gradient = 0
+def gradient_descent(m_now, c_now, points, L):
+    m_gradient = points.iloc[len(points)//2].X
+    c_gradient = points.iloc[len(points)//2].Y
     
     n = len(points)
     
@@ -29,25 +30,25 @@ def gradient_descent(m_now, b_now, points, L):
         x = points.iloc[i].X
         y = points.iloc[i].Y
         
-        m_gradient += -(2/n) * x * (y - (m_now * x * b_now))
-        b_gradient += -(2/n) * (y - (m_now * x * b_now))
+        m_gradient += -(2/n) * x * (y - (m_now * x + c_now))
+        c_gradient += -(2/n) * (y - (m_now * x + c_now))
     
     m = m_now - m_gradient * L
-    b = b_now - b_gradient * L
+    c = c_now - c_gradient * L
     
-    return m, b
+    return m, c
 
-m, b, L = 0, 0, 0.0001
+m, c, L = 0, 0, 0.0001
 epochs = 1000
 
 for i in range(epochs):
-    m, b = gradient_descent(m, b, data2, L)
+    m, c = gradient_descent(m, c, data2, L)
     
     if epochs % 50 == 0:
         print(f'Epochs = {i}')
 
 
-print(m, b)
+print(m, c)
 plt.scatter(data2.X, data2.Y, color='black')
-plt.plot(list(range(0, 30)), [m * x * b for x in range(0, 30)], color='red')
+plt.plot(list(range(0, 30)), [m * x + c for x in range(0, 30)], color='red')
 plt.show()
